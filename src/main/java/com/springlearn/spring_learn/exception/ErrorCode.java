c2dd0f7308764a13769094727cf6aabb2073e0d2
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-09-26 23:58:07 +0700

    exception manage, response with httpcode

--- src/main/java/com/springlearn/spring_learn/exception/ErrorCode.java
+++ src/main/java/com/springlearn/spring_learn/exception/ErrorCode.java
@@ -1,36 +1,30 @@
 package com.springlearn.spring_learn.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    USER_EXISTED(1002, "user existed"),
-    UNCATEGORIZED_ERROR(9999, "uncategorized error"),
-    UNAUTHENTICATED_ERROR(1006, "unauthenticated error"),
-    INVALID_KEY(1001, "invalid message key"),
-    USERNAME_INVALID(1003, "username must be at least 3 characters"),
-    USER_NOT_EXISTED(1005, "user not exist"),
-    PASSWORD_INVALID(1004, "password must be at least 8 characters");
+    USER_EXISTED(1002, "user existed", HttpStatus.BAD_REQUEST),
+    UNCATEGORIZED_ERROR(9999, "uncategorized", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHENTICATED_ERROR(1006, "unauthenticated", HttpStatus.UNAUTHORIZED),
+    INVALID_KEY(1001, "invalid message key", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1005, "user not exist", HttpStatus.NOT_FOUND),
+    PASSWORD_INVALID(1004, "password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN);
 
     private int code;
 
     private String message;
 
-    ErrorCode(int code, String message) {
+    private HttpStatusCode statusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+        this.statusCode = statusCode;
     }
 }
