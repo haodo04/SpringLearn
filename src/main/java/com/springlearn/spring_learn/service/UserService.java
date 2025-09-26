c6ec4ecaebfcfc62a2a810b8c5776c728846ddfe
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-09-26 22:30:28 +0700

    authorization with PreAuthorize and PostAuthorize

--- src/main/java/com/springlearn/spring_learn/service/UserService.java
+++ src/main/java/com/springlearn/spring_learn/service/UserService.java
@@ -12,16 +12,22 @@ import com.springlearn.spring_learn.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserService {
 
     UserRepository userRepository;
@@ -40,11 +46,24 @@ public class UserService {
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
+    public UserResponse getMyInfo() {
+        var context = SecurityContextHolder.getContext();
+        String name = context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(name)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        return userMapper.toUserResponse(user);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getUsers() {
+        log.info("In method get Users");
         return userRepository.findAll();
     }
 
+    @PostAuthorize("returnObject.username == authentication.name")
     public UserResponse getUser(Long id) {
+        log.info("In method get user by id");
         return userMapper.toUserResponse(userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found")));
     }
 
